85dfef39fc855023c0cf8c24393668708eb2a032
dennyellen <tuqian@hotmail.com>
2018-04-19 00:35:59 +0800

    Fix script

--- script/http_test.php
+++ script/http_test.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once(__DIR__.'/HttpUtils.class.php');
+include_once('../utils/HttpUtils.class.php');
 
 // $output = HttpUtils::httpGet("http://localhost:8080");
 // var_dump($output);
