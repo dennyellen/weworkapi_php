d92290876dfe55e824c4afef24f0356a222e4748
dennyellen <tuqian@hotmail.com>
2018-04-18 23:45:57 +0800

    Add InternalError

--- utils/error.inc.php
+++ utils/error.inc.php
@@ -64,3 +64,16 @@ class QyApiError extends Exception
         return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
     }
 }
+
+class InternalError extends Exception
+{
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
